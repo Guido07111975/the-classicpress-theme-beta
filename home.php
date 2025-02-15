fcd7890cf67a19130146fab9c437257ee9550d97
Guido07111975 <gjvdleest@yahoo.com>
2025-02-15 12:45:35 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying latest posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+
+get_header();
+?>
+
+	<div id="primary">
+		<main id="main" class="page-main" role="main">
+			<div class="post-list">
+
+				<?php
+				if ( have_posts() ) :
+
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						get_template_part( 'template-parts/content', 'list' );
+
+					endwhile;
+
+					the_posts_navigation();
+
+				else:
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+				?>
+
+			</div>
+		</main><!-- #main -->
+
+		<?php get_sidebar(); ?>
+
+	</div><!-- #primary -->
+
+<?php
+get_footer();
