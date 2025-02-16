766f37a8543d970198b8ddca8a01300eda2088a5
Guido07111975 <gjvdleest@yahoo.com>
2025-02-16 13:10:48 +0100

    Add files via upload

--- template-parts/content.php
+++ template-parts/content.php
@@ -47,4 +47,25 @@
 		?>
 	</div>
 
+	<?php
+	if ( get_edit_post_link() ) :
+		edit_post_link(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'the-classicpress-theme' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			),
+			'<div class="edit-link">',
+			'</div>'
+		);
+	endif;
+	?>
+
 </article><!-- #post-<?php the_ID(); ?> -->
